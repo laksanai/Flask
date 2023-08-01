d56cbff6eec78a899ec11e34abd44a5df183b70b
Laksanai <laksanai555@hotmail.com>
2023-08-01 22:04:20 +0700

    Dinymic Route

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello Flask framework</h1>"
 
+@app.route('/user/<name>') #Here dinymic route
+def menber(name):
+    return "<h1>สวัสดีสมาชิก: {}</h1>".format(name)
+
 if __name__ == "__main__":
     app.run()
 
